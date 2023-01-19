ea0aeddbfd79f4b124a990883df19a24d205762f
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-01-19 16:43:39 -0300

    Documentação da API com o Swagger e incluindo o arquivo do Insomnia

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/routes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 
@@ -10,5 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(process.env.PORT);
