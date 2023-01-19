43fdbbabb393d117eacd6abad26f24f1df013111
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-01-19 12:38:54 -0300

    Endpoint de Visualizar um Carro e pequena correção no Endpoint de Listar Carros

--- backend/src/controllers/cars.js
+++ backend/src/controllers/cars.js
@@ -17,7 +17,7 @@ const registerCar = async (req, res) => {
         if (!registeredCar[0]) {
             return res.status(500).json({ mensagem: "Erro interno do servidor." });
         }
-        
+
         const carData = {
             id: registeredCar[0].id,
             nome: registeredCar[0].nome,
@@ -42,14 +42,64 @@ const listCars = async (req, res) => {
     try {
         const cars = await knex('carros');
 
-        return res.status(200).json(cars);
+        let carsData = [];
+
+        for (const car of cars) {
+            const carData = {
+                id: car.id,
+                nome: car.nome,
+                marca: car.marca,
+                ano_fabricacao: car.ano_fabricacao,
+                descricao: car.descricao,
+                dono: {
+                    nome: car.nome_dono,
+                    email: car.email_dono,
+                    telefone: car.telefone_dono
+                }
+            };
+
+            carsData.push(carData);
+        }
+
+        return res.status(200).json(carsData);
 
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor." });
     }
 }
 
+const viewCar = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const car = await knex('carros').where({ id }).first();
+
+        if (!car) {
+            return res.status(404).json({ mensagem: "O carro não existe." });
+        }
+
+        const carData = {
+            nome: car.nome,
+            marca: car.marca,
+            ano_fabricacao: car.ano_fabricacao,
+            descricao: car.descricao,
+            dono: {
+                nome: car.nome_dono,
+                email: car.email_dono,
+                telefone: car.telefone_dono
+            }
+        };
+        
+        return res.status(200).json(carData);
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: "Erro interno do servidor." });
+    }
+}
+
 module.exports = {
     registerCar,
-    listCars
+    listCars,
+    viewCar
 }
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const validateRequest = require('../middlewares/validateRequest');
 const carSchema = require('../validations/carSchema');
-const { registerCar, listCars } = require('../controllers/cars');
+const { registerCar, listCars, viewCar } = require('../controllers/cars');
 
 const routes = express();
 
 routes.post('/carro', validateRequest(carSchema), registerCar);
 routes.get('/carros', listCars);
+routes.get('/carros/:id', viewCar);
 
 module.exports = routes;
