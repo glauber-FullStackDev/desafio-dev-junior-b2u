821196dc08617b95bda6200fc8b5f8aa50ecca31
thifelipesilva <thiagophsilva@hotmail.com>
2023-01-19 15:50:35 -0300

    Configurando setup da api

---
+++ api-venda-carros/src/appServer.ts
@@ -0,0 +1,17 @@
+import { Server } from "@overnightjs/core";
+import bodyParser from 'body-parser';
+import cors from "cors";
+
+export class AppServer extends Server {
+    
+    constructor() {
+        super();
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(cors());
+    }
+
+    public start(port: number) {
+        this.app.listen(port, () => console.log('Servidor Criado'))
+    }
+}
---
+++ api-venda-carros/src/index.ts
@@ -0,0 +1,5 @@
+import { AppServer } from "./appServer";
+
+const server = new AppServer();
+const port = 4000;
+server.start(port);
