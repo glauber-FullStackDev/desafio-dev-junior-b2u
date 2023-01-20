e6b46c866c1eee77a7fca898240ac628a50d1309
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-01-20 16:26:20 -0300

    Estilização da página e do card

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -6,7 +6,19 @@ function Home() {
     <div className="container">
       <Header />
       <div className="content-home">
-        <h1>Home</h1>
+        <h1>Lista de anuncios de carros</h1>
+        <div className="container-cards">
+            <div className="content-card">
+              <h2>Camaro</h2>
+              <strong>Marca: Chevrolet</strong>
+              <strong>Ano de Fabricação: 2015</strong>
+              <span>Descrição: Poucos KM rodados...</span>
+              <div className="container-buttons">
+                <button className="btn-edit">Editar</button>
+                <button className="btn-delete">Excluir</button>
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   );
