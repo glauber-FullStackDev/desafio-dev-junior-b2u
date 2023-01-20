597ac5cc09a4fcec61a0927304fc2bacbd7cd867
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-01-20 13:05:34 -0300

    Criação inicial do projeto frontend com React

---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,15 @@
+import Header from '../../components/Header';
+import './styles.css';
+
+function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <div className="content-home">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
