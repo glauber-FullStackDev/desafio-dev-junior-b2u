053c24e1e84da4d8a7464fd8228f46fc3dc71249
DavidBassouto <davidbassouto@hotmail.com>
2023-01-20 16:06:29 -0300

    fix: code layout improvement

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import app from "./app";
 import AppDataSource from "./data-source";
-import "dotenv/config"
+import "dotenv/config";
 
 (async () => {
-    const PORT="8000"
+  const PORT = "8000";
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(process.env.PORT || PORT, () => {
-        console.log(`Servidor executado no localhost ${PORT}`)
-    })    
-})()
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Servidor executado no localhost ${PORT}`);
+  });
+})();
