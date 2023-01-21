9aadfd5bb33222f48a8825d058baaffdbac29b00
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-21 17:17:38 -0300

    feat: criado o componente form

---
+++ frontend/src/components/form/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ContainerForm = styled.form`
+  width: 50%;
+  padding: 3em;
+  background-color: #ffffff;
+  border-radius: 10px;
+`;
