c780779133da6b5ef02f302e83f7733553d879f1
adriel <adrieldev@outlook.com>
2023-01-21 20:50:40 -0300

    feat-footer

--- frontend/bitcar/src/App.jsx
+++ frontend/bitcar/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Dashboard from "./components/Dashboard"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="">
         <Header/>
         <Dashboard/>
+        <Footer/>
     </div>
   )
 }
