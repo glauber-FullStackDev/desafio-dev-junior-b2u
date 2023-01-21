bbebed9a75f1b206fdc92ede75d5eff7992c177b
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-21 19:18:48 -0300

    feat: reutilizado o componente model no card do carro

--- frontend/src/components/card/index.tsx
+++ frontend/src/components/card/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import Form from "../../components/form";
+import Modal from "@mui/material/Modal";
+import { Box } from "@mui/material";
 
 import { ContainerCard, Title, Description, Wrapper, SubTitle } from "./styles";
 import { ICars } from "../../interface/ICars";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
 
 const CardCar = ({
   carId,
@@ -13,6 +25,10 @@ const CardCar = ({
   carDescription,
   carBrands,
 }: ICars) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <ContainerCard key={index}>
       <CardContent>
@@ -24,7 +40,14 @@ const CardCar = ({
         </Wrapper>
       </CardContent>
       <CardActions>
-        <Button size="small">EDIT</Button>
+        <Button size="small" onClick={handleOpen}>
+          EDIT
+        </Button>
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={style}>
+            <Form handleClose={handleClose} />
+          </Box>
+        </Modal>
         <Button size="small">DELETE</Button>
       </CardActions>
     </ContainerCard>
