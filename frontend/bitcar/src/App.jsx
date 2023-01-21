43cbe2f124bdd11895ceb4f906ccd8bc39697d73
adriel <adrieldev@outlook.com>
2023-01-21 19:19:16 -0300

    feat-create-themes-tailwind

--- frontend/bitcar/src/App.jsx
+++ frontend/bitcar/src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <div className="App">
-        teste
+    <div className="">
+        <Header/>
     </div>
   )
 }
--- frontend/bitcar/tailwind.config.cjs
+++ frontend/bitcar/tailwind.config.cjs
@@ -1,10 +1,33 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors:{
+      gray:{
+        one:'#0A0A0A',
+        two:'#1A1A1A',
+        three:'#292929',
+        four:'#3D3D3D',
+        five:'#4D4D4D',
+        six:'#616161',
+        seven:'#7A7A7A',
+        eight:'#8F8F8F',
+        nine:'#A8A8A8',
+        ten:'#C7C7C7',
+        eleven:'#E0E0E0',
+        twelve:'#F5F5F5',
+
+      },
+      primary:{
+        dark:'#0817FF',
+        light:'#4186CB',
+        medium:'#003B79'
+
+
+      }
+    },
     extend: {},
   },
   plugins: [],
