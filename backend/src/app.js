ada3ec2bd2f1eb04c488b34749cd443c0791c148
Jake Chien <jake_chien@outlook.com>
2023-01-22 17:34:52 -0300

    chore: add's cors in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/errorHandler');
 const carrosRoute = require('./routes/carrosRoute');
 const donosRoute = require('./routes/donosRoute');
@@ -6,6 +7,7 @@ const donosRoute = require('./routes/donosRoute');
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/carros', carrosRoute);
 app.use('/donos', donosRoute);
 app.use(errorHandler);
