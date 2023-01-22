35a72224d028bad8202eff9e762988d730163dcb
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-22 19:49:19 -0300

    feat: criando o repostiry, service e controller para criar uma nova brand

--- backend/src/routes/brands.ts
+++ backend/src/routes/brands.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import allBrandsController from "../controllers/brands/all-brands-controller";
+import createBrandController from "../controllers/brands/create-brand-controller";
 
 const routeBrands = Router();
 
 routeBrands.get("/brands", allBrandsController);
+routeBrands.post("/brands", createBrandController);
 
 export { routeBrands };
