583ef4e36adfef8f8e1c3129ce18815f29fc31b0
Jake Chien <jake_chien@outlook.com>
2023-01-22 17:31:10 -0300

    feat: change mock to fetch cars on frontend

--- frontend/src/pages/Cars.js
+++ frontend/src/pages/Cars.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import CarsCard from '../components/CarsCard';
-import cars from '../mock/carsMock';
+import Loading from '../components/Loading';
 
 function Cars() {
+  const [showError, setShowError] = useState('');
+  const [allCarsFetched, setAllCarsFetched] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const getAllCars = async () => {
+      const response = await fetch('http://localhost:3001/carros');
+      const data = await response.json();
+      if (data.message) return setShowError(data.message);
+      if (isMounted) return setAllCarsFetched(data);
+    }
+    getAllCars()
+    return () => { isMounted = false }
+  }, [])
+
   return (
     <>
-      {cars.map((car) => (
+      {allCarsFetched ? allCarsFetched.map((car) => (
         <Link to={`/cars/${car.id}`}>
           <CarsCard
             key={car.id}
@@ -16,7 +31,8 @@ function Cars() {
             owner={car.owner}
           />
         </Link>
-      ))}
+      )) : <Loading />}
+      <span>{showError && showError}</span>
     </>
   )
 }
