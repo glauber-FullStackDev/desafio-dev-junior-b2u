26bb276c91860ada970f7a55c088089d96fd8614
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-22 22:44:47 -0300

    feat: feita a requisição do usuários na tabela de usuários

--- frontend/src/components/table/index.tsx
+++ frontend/src/components/table/index.tsx
@@ -3,11 +3,12 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { IUsers } from "../../interface/IUsers";
 
 import RowsTable from "../rowsTable";
 import { ContainerTable } from "./styles";
 
-const TableInfo = () => {
+const TableInfo = ({users}: IUsers) => {
   return (
     <ContainerTable>
       <Table>
@@ -20,7 +21,7 @@ const TableInfo = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <RowsTable />
+          <RowsTable users={users} />
         </TableBody>
       </Table>
     </ContainerTable>
--- frontend/src/pages/users/index.tsx
+++ frontend/src/pages/users/index.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 import Header from "../../components/header";
 import TableInfo from "../../components/table";
+import getAllUsers from "../../services/users/get-all-users";
+import { IUsers } from "../../interface/IUsers";
+
 import { Container } from "./styles";
 
 const Users = () => {
+  const [users, setUsers] = useState<IUsers[]>([]);
+
+  const getUsers = async () => {
+    const response = await getAllUsers();
+    if (response.erro) {
+      toast.error(response.error);
+    }
+    setUsers(response);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
       <Header />
       <Container>
-        <TableInfo />
+        <TableInfo users={users}/>
       </Container>
     </>
   );
