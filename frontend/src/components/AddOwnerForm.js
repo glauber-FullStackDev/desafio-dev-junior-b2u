4fcbdea095905ea42fb5e95717f8334b7e209a6f
Jake Chien <jake_chien@outlook.com>
2023-01-23 20:04:34 -0300

    feat: add's inputs validator on add owner form

--- frontend/src/components/AddOwnerForm.js
+++ frontend/src/components/AddOwnerForm.js
@@ -1,13 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import CarContext from '../context/CarContext';
 
 function AddOwnerForm() {
   const { setOwnerInfos } = useContext(CarContext)
 
-  const [ownerName, setOwnerName] = useState();
-  const [ownerEmail, setOwnerEmail] = useState();
-  const [ownerTelephone, setOwnerTelephone] = useState();
+  const [ownerName, setOwnerName] = useState('');
+  const [ownerEmail, setOwnerEmail] = useState('');
+  const [ownerTelephone, setOwnerTelephone] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const history = useHistory();
 
@@ -21,6 +22,34 @@ function AddOwnerForm() {
     setOwnerInfos(ownerInfos)
     history.push('/add/car')
   }
+
+  useEffect(() => {
+    const nameValidator = () => {
+      const MINIMUM_NAME_LENGTH = 6;
+      const isNameValid = ownerName.length >= MINIMUM_NAME_LENGTH;
+      return isNameValid;
+    }
+    const emailValidator = () => {
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      const isEmailValid = emailRegex.test(ownerEmail);
+      return isEmailValid;
+    }
+    const telephoneValidator = () => {
+      const MAXIMUM_CEL_NUMBER = 11;
+      const isNumberLengthValid = ownerTelephone.length === MAXIMUM_CEL_NUMBER;
+      return isNumberLengthValid;
+    }
+    const handleButtonControl = () => {
+      const isNameValid = nameValidator();
+      const isEmailValid = emailValidator();
+      const isNumberLengthValid = telephoneValidator();
+      if (isNameValid && isEmailValid && isNumberLengthValid) {
+        return setIsButtonDisabled(false);
+      }
+      return setIsButtonDisabled(true);
+    }
+    handleButtonControl();
+  })
 
   const handleInputsControl = ({ target: { name, value } }) => {
     const options = {
@@ -62,6 +91,7 @@ function AddOwnerForm() {
         <button
           type='submit'
           onClick={handleClick}
+          disabled={isButtonDisabled}
         >
           Cadastrar carro
         </button>
