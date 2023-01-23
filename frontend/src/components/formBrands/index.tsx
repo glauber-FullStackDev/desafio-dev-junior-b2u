e4e3f2e60fc8001ee634e5f14af52e4fc19b8c67
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-23 19:27:53 -0300

    feat: criado o componente formBrand

---
+++ frontend/src/components/formBrands/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+import {
+  ButtonClose,
+  Container,
+  ContainerForm,
+  Wrapper,
+  WrapperClose,
+} from "./styles";
+
+const initialState = {
+  id: "",
+  name: "",
+};
+
+const FormBrands = () => {
+  const [brand, setBrand] = useState(initialState);
+  return (
+    <Container>
+      <ContainerForm>
+        <Wrapper>
+          <ButtonClose>Close</ButtonClose>
+        </Wrapper>
+
+        <Typography variant="h6">User information</Typography>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              label="Name"
+              fullWidth
+              variant="standard"
+              value={brand.name}
+              onChange={(event) =>
+                setBrand({ ...brand, name: event.target.value })
+              }
+            />
+          </Grid>
+        </Grid>
+
+        <WrapperClose>
+          <ButtonClose type="submit">Register</ButtonClose>
+        </WrapperClose>
+        
+      </ContainerForm>
+    </Container>
+  );
+};
+
+export default FormBrands;
